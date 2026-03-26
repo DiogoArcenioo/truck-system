0a4bacaccdba0553fa994c7218a0c12b335ac5c1
DiogoArcenioo <diogoleonardoa@gmail.com>
2026-03-26 11:11:11 -0300

    LOGIN EMAIL SEMPRE TOUPPERCASE

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,6 +1,10 @@
+import { Transform } from 'class-transformer';
 import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class LoginDto {
+  @Transform(({ value }) =>
+    typeof value === 'string' ? value.trim().toUpperCase() : value,
+  )
   @IsEmail()
   @MaxLength(150)
   email!: string;
